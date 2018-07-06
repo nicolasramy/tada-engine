e25169c3117178694bf4f07d64785db77d33afe2
Nicolas RAMY <nicolas.ramy@darkelda.com>
2018-07-06 11:59:12 +0200

    change configuration for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,20 @@
 SAMPLE_CONFIGURATION = """
 [tada-engine]
 host = 127.0.0.1
-frontend_port = 5501
-backend_port = 5601
-monitoring_port = 5701
+
+frontend_port = 5601
+backend_port = 5602
+monitoring_port = 5603
+http_port =  5680
 
 log_level = DEBUG
-log_file = /var/log/tada-engine--test.log
+log_file = /tmp/var/log/tada-engine--test.log
 
-proxy_pid_file = /var/run/tada-engine-proxy--test.pid
-master_pid_file = /var/run/tada-engine-master--test.pid
-minion_pid_file = /var/run/tada-engine-minion--test.pid
+proxy_pid_file = /tmp/var/run/tada-engine-proxy--test.pid
+master_pid_file = /tmp/var/run/tada-engine-master--test.pid
+minion_pid_file = /tmp/var/run/tada-engine-minion--test.pid
+
+data_path = /tmp/var/lib/tada-engine/
+minion_cache = /tmp/var/lib/tada-engine/minion
+master_cache = /tmp/var/lib/tada-engine/master
 """
