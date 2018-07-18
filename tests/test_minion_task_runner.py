e1e06f2254c72a34466627681a9f7caccd9eeb26
Nicolas RAMY <nicolas.ramy@darkelda.com>
2018-07-18 16:20:49 +0200

    implement taskrunners

--- tests/test_minion_task_runner.py
+++ tests/test_minion_task_runner.py
@@ -3,19 +3,19 @@
 import os
 import unittest
 
-from tada_engine.minion import TaskRunner
+from tada_engine.minion import AbstractTaskRunner, SimpleTaskRunner, AsyncTaskRunner, ConcurrentTaskRunner
 
 from . import add
 
 
-class TaskRunnerTest(unittest.TestCase):
+class AbstractTaskRunnerTest(unittest.TestCase):
 
     INPUT = 'tests/input.csv'
     OUTPUT = 'tests/output.csv'
     TASK = add
 
     def test_hash_string(self):
-        task_runner = TaskRunner(self.TASK, self.INPUT, self.OUTPUT)
+        task_runner = AbstractTaskRunner(self.TASK, self.INPUT, self.OUTPUT)
         self.assertEqual(
             task_runner._generate_key('Hello World'),
             '2c74fd17edafd80e8447b0d46741ee243b7eb74dd2149a0ab1b9246fb30382f27e853d8585719e0e67cbda0daa8f51671064615d64'
@@ -23,7 +23,7 @@ class TaskRunnerTest(unittest.TestCase):
         )
 
     def test_hash_not_string(self):
-        task_runner = TaskRunner(self.TASK, self.INPUT, self.OUTPUT)
+        task_runner = AbstractTaskRunner(self.TASK, self.INPUT, self.OUTPUT)
         self.assertEqual(
             task_runner._generate_key({"a": 1, "b": 2}),
             'b5da773f945631ed9943f66ab28641439d8895e350fb1fb9e21377bc63cd546bb68a5db808c57f846ddb195def323b315fe8917213'
@@ -31,14 +31,55 @@ class TaskRunnerTest(unittest.TestCase):
         )
 
     def test_get_extension_input(self):
-        task_runner = TaskRunner(self.TASK, self.INPUT, self.OUTPUT)
+        task_runner = AbstractTaskRunner(self.TASK, self.INPUT, self.OUTPUT)
         self.assertEqual(
             task_runner._get_extension(),
             'text/plain'
         )
 
+
+class SimpleTaskRunnerTest(unittest.TestCase):
+
+    INPUT = 'tests/input.csv'
+    OUTPUT = 'tests/output.csv'
+    TASK = add
+
     def test_execute(self):
-        task_runner = TaskRunner(self.TASK, self.INPUT, self.OUTPUT)
+        task_runner = SimpleTaskRunner(self.TASK, self.INPUT, self.OUTPUT)
+
+        task_runner.execute()
+
+        self.assertEqual(
+            os.path.getsize(self.OUTPUT),
+            3277
+        )
+
+
+class AsyncTaskRunnerTest(unittest.TestCase):
+
+    INPUT = 'tests/input.csv'
+    OUTPUT = 'tests/output.csv'
+    TASK = add
+
+    def test_execute(self):
+        task_runner = AsyncTaskRunner(self.TASK, self.INPUT, self.OUTPUT)
+
+        task_runner.execute()
+
+        self.assertEqual(
+            os.path.getsize(self.OUTPUT),
+            3277
+        )
+
+
+class ConcurrentTaskRunnerTest(unittest.TestCase):
+
+    INPUT = 'tests/input.csv'
+    OUTPUT = 'tests/output.csv'
+    TASK = add
+
+    def test_execute(self):
+        task_runner = ConcurrentTaskRunner(self.TASK, self.INPUT, self.OUTPUT)
 
         task_runner.execute()
 
